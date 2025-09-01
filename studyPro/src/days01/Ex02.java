113cceba686e4330e1340665f721ebd278b67dd5
BBDUYU <tmdwhddh@gmail.com>
2025-09-01 17:22:45 +0900

    Ex01.java 복습

--- studyPro/src/days01/Ex02.java
+++ studyPro/src/days01/Ex02.java
@@ -3,10 +3,9 @@ package days01;
 public class Ex02 {
 
 	public static void main(String[] args) {
-		System.out.println("복습");
-		System.out.println("복습");
-		System.out.println("복습");
-		System.out.println("복습");
+		System.out.println("교육원 복습");
+		System.out.println("교육원 복습");
+		System.out.println("교육원 복습");
 	}
 
 }
